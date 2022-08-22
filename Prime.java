ad213530648e96eb6c77d9630e90669158f18c7d
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2022-08-22 23:26:12 +0530

    Prime Number or Not

--- Prime.java
+++ Prime.java
@@ -5,12 +5,15 @@ public class Prime {
         System.out.print("Enter a number to check whether it is prime or not ");
         Scanner in = new Scanner(System.in) ;
         int num = in.nextInt();
-        int i ;
+        int i ,flag ;
+        flag = 0 ;
         for ( i = 2; i<=num-1 ; i++){
             if(num%i==0)
+                System.out.println("Not a prime");
+                flag = 1 ;
                 break;
         }
-        if ( num==i) System.out.println("it is a prime number ");
+        if ( flag==0) System.out.println("it is a prime number ");
         else System.out.println("Not a prime ");
     }
 }
