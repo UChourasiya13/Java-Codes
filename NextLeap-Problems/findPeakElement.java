e471fc12a5d4cdc13fb36fbde0172c474cee9ae1
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2023-08-24 23:36:12 +0530

    Create findPeakElement.java

---
+++ NextLeap-Problems/findPeakElement.java
@@ -0,0 +1,24 @@
+class Solution {
+	public int findPeakElement(int[] nums) {
+		// Your code goes here
+		int []arr = nums;
+		int n = arr.length;
+		if(n==1)return 0;
+		if(nums[0]>nums[1])return 0;
+		if(nums[n-1]>nums[n-2]) return n-1;
+		int low = 1;
+		int high = n-2;
+		while(low<=high){
+			int mid = (low+high)/2;
+			if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])return mid;
+			else if(arr[mid]>arr[mid-1]){
+				low = mid+1;
+			}
+			else if(arr[mid]>arr[mid+1]){
+				high = mid-1;
+			}
+			else{
+				low = mid+1;
+			}
+		}
+		return -1;
