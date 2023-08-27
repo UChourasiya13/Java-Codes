136753a0dc2204739fd273a894983b31b0ed026c
Ujjawal Chourasiya <89144606+UChourasiya13@users.noreply.github.com>
2023-08-27 11:37:18 +0530

    Bitwise Odd Even

---
+++ NextLeap-Problems/BitwiseOddEven.java
@@ -0,0 +1,11 @@
+class Solution {
+	public String checkNumber(int num) {
+		// Your code goes here
+		String ans = "odd" ;
+		if((num&1)!=0) ans="odd";
+		else  ans= "even";
+			
+	return ans;
+	}
+	
+}
